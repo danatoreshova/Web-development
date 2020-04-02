860b839694ad01939613284ecff0622ab5707990
danatoreshova <dana.toreshova@bk.ru>
2020-04-02 22:13:21 +0600

    commit 1

---
+++ api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.FloatField()
+    description = models.TextField(default="")
+    count = models.IntegerField()
+    category = models.ForeignKey(Category, default=None, on_delete=models.CASCADE)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count
+        }
